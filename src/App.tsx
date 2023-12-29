3d024071978b9b0edfa681bfbefacf3a887f84e3
SyedMuzamil <1821182-SyedMuzamil@users.noreply.replit.com>
2023-12-29 15:20:52 +0000

    Added the ability to get the media stream

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,53 @@
 import './App.css'
 
+import { useEffect, createRef, useRef } from 'react'
+
+const constrains = {
+	video: true,
+	audio: true
+}
+
 export default function App() {
-  return (
-    <main>
-      React ⚛️ + Vite ⚡ + Replit 🌀
-    </main>
-  )
+	const ref = createRef<HTMLVideoElement>()
+	const streamRef = useRef<MediaStream | null>()
+
+	useEffect(() => {
+		// Need to get the media stream somehow
+		console.log({ streamRef })
+	}, [streamRef])
+
+
+	const getStreams = async () => {
+		try {
+			const streams = await navigator.mediaDevices.getUserMedia(constrains)
+			if (streamRef.current) {
+				streamRef.current = streams
+			}
+			if (ref.current) {
+				ref.current.srcObject = streams
+				ref.current.autoplay = true
+			}
+		} catch (err) {
+			alert(err)
+		}
+	}
+
+	const closeStream = () => {
+		if (streamRef.current) {
+			streamRef.current.getTracks().forEach(track => track.stop())
+			// Todo: streamRef.current = null
+		}
+	}
+
+	return (
+		<main>
+			Google Meet Clone
+
+			<video ref={ref} src={streamRef.current} playsinline autoplay={true} controls={false} />
+			<div>
+				<button onClick={getStreams}>Get Video</button>
+				<button onClick={closeStream}>Stop Video</button>
+			</div>
+		</main>
+	)
 }
